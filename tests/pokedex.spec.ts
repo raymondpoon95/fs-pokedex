47809955001a8417ebd9db8ccf468730fbadf422
raymondPoonReciteMe <ray.poon@reciteme.com>
2026-05-20 19:26:39 +0100

    e2e test fix

--- tests/pokedex.spec.ts
+++ tests/pokedex.spec.ts
@@ -1,13 +1,13 @@
-import { test, expect } from '@playwright/test';
+const { describe, test, expect } = require('@playwright/test')
 
-test.describe('Pokedex', () => {
-  test('front page can be opened', async ({ page }) => {
+describe('Pokedex', () => {
+  test('front page can be opened', async (page: any) => {
     await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 
-  test('navigates to pokemon details page', async({page}) => {
+  test('navigates to pokemon details page', async(page: any) => {
     await page.goto('/')
     await page.getByRole('link', { name: 'ivysaur' }).click();
 
