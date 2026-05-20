94fac654b1234dc144d4c39d493c85016d2ea055
raymondPoonReciteMe <ray.poon@reciteme.com>
2026-05-20 19:31:03 +0100

    update run e2e script

--- tests/pokedex.spec.ts
+++ tests/pokedex.spec.ts
@@ -1,13 +1,13 @@
 const { describe, test, expect } = require('@playwright/test')
 
 describe('Pokedex', () => {
-  test('front page can be opened', async (page: any) => {
+  test('front page can be opened', async ({page}) => {
     await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
     await expect(page.getByText('Pokémon and Pokémon character names are trademarks of Nintendo.')).toBeVisible()
   })
 
-  test('navigates to pokemon details page', async(page: any) => {
+  test('navigates to pokemon details page', async({page}) => {
     await page.goto('/')
     await page.getByRole('link', { name: 'ivysaur' }).click();
 
