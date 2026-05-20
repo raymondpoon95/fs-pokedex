a832d2f5e3f8283b72afcbbb602858aab7c27bf8
raymondPoonReciteMe <ray.poon@reciteme.com>
2026-05-20 19:46:07 +0100

    e2e retry

--- tests/pokedex.test.ts
+++ tests/pokedex.test.ts
@@ -1,4 +1,4 @@
-const { test, describe, expect, beforeEach } = require('@playwright/test')
+import { test, expect } from '@playwright/test';
 
 test.describe('Pokedex', () => {
   test('front page can be opened', async ({page}) => {
