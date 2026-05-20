75e2adf4419ae1df05d0652528ef64cee1ce0204
raymondPoonReciteMe <ray.poon@reciteme.com>
2026-05-20 18:36:18 +0100

    lint fixes

--- eslint.config.js
+++ eslint.config.js
@@ -10,7 +10,8 @@ module.exports = [
       'eslint.config.js',
       '.eslintrc.js',
       'node_modules/**',
-      'dist/**'
+      'dist/**',
+      'jest.setup.js'
     ]
   },
   js.configs.recommended,
