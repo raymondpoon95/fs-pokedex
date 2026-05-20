b3bf6e6962b37eedff5b486a23311acd23aaa310
raymondPoonReciteMe <ray.poon@reciteme.com>
2026-05-20 19:24:37 +0100

    e2e test fix

--- tests/pokedex.spec.ts
+++ tests/pokedex.spec.ts
@@ -1,7 +1,6 @@
-const { describe, beforeEach } = require('@playwright/test')
 import { test, expect } from '@playwright/test';
 
-describe('Pokedex', () => {
+test.describe('Pokedex', () => {
   test('front page can be opened', async ({ page }) => {
     await page.goto('/')
     await expect(page.getByText('ivysaur')).toBeVisible()
