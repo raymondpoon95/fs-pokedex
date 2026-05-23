a91229a2b7720c1ff8e7c62bcd49f755d8592b40
raymondPoonReciteMe <ray.poon@reciteme.com>
2026-05-24 00:53:06 +0100

    version api call

--- app.js
+++ app.js
@@ -1,14 +1,20 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001
+const PORT = process.env.PORT || 5001;
 
-app.use(express.static('dist'))
+app.use(cors());
+app.use(express.static("dist"));
+
+app.get("/version", (req, res) => {
+  res.send("1"); // change this string to ensure a new version deployed
+});
 
 const start = async () => {
-  await app.listen(PORT)
-  console.log(`server started on port ${PORT}`)
-}
+  await app.listen(PORT);
+  console.log(`server started on port ${PORT}`);
+};
 
-start()
+start();
