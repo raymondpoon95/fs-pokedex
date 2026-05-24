53c3a4e9f2bd8c657fcbd884761ac78e48fcf5ca
Raymond Poon <raymondpoon95@hotmail.co.uk>
2026-05-24 01:56:13 +0100

    lint fixes

--- app.js
+++ app.js
@@ -1,24 +1,24 @@
-const express = require("express");
-const cors = require("cors");
-const app = express();
+const express = require('express')
+const cors = require('cors')
+const app = express()
 
 // get the port from env variable
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 5001
 
-app.use(cors());
-app.use(express.static("dist"));
+app.use(cors())
+app.use(express.static('dist'))
 
-app.get("/version", (req, res) => {
-  res.send("1"); // change this string to ensure a new version deployed
-});
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
 
-app.get("/health", (req, res) => {
-  res.status(200).send("ok");
-});
+app.get('/health', (req, res) => {
+  res.status(200).send('ok')
+})
 
 const start = async () => {
-  await app.listen(PORT);
-  console.log(`server started on port ${PORT}`);
-};
+  await app.listen(PORT)
+  console.log(`server started on port ${PORT}`)
+}
 
-start();
+start()
